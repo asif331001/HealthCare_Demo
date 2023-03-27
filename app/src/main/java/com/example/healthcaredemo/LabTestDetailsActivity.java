8b93372f7bd0410261ec1ce6227d45f82eea9e07
asif331001 <asif95331001@gmail.com>
2023-03-27 16:08:23 +0600

    test and package activity is added

--- app/src/main/java/com/example/healthcaredemo/LabTestDetailsActivity.java
+++ app/src/main/java/com/example/healthcaredemo/LabTestDetailsActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LabTestDetailsActivity extends AppCompatActivity {
 
@@ -31,5 +33,13 @@ public class LabTestDetailsActivity extends AppCompatActivity {
         tvPackageName.setText(intent.getStringExtra("text1"));
         edDetails.setText(intent.getStringExtra("text2"));
         tvTotalCost.setText("Total Cost: "+intent.getStringExtra("text3")+"/-");
+
+        btnLDCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"Information is Stored", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
